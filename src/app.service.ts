b33167ff82fe9079cfa605bace4ace4e6c07ec08
musmankhalil <musmankhalil@gmail.com>
2026-03-13 05:49:50 +0500

    chore: append ' Test' to the getHello return string.

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello from LaunchPad!';
+    return 'Hello from LaunchPad! Test';
   }
 }
