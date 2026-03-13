3eeb1057a5e0dd2a806ec0ac471e605453f41f73
musmankhalil <musmankhalil@gmail.com>
2026-03-13 06:13:44 +0500

    chore: update hello message text

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello from LaunchPad! Test';
+    return 'Hello from LaunchPad! Test1';
   }
 }
